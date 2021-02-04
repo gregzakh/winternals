edb106b282de3ffd6491009e761f802bd6cd1c98
gregzakh <grishanz@yandex.com>
2021-02-05 00:05:50 +0500

    M virt/test.c

--- virt/test.c
+++ virt/test.c
@@ -4,14 +4,13 @@
 typedef unsigned char byte;
 typedef unsigned long uint;
 
-float getversion(void) {
+uint getversion(void) {
   uint *ver = *(uint (*)[2])0x7FFE026C;
-  int  decs = log10(ver[1]) + 1;
-  return ver[0] + ver[1] * pow(10.0, -decs);
+  return ver[0] * 10 + ver[1];
 }
 
 int main(void) {
-  if (6.2 <= getversion()) {
+  if (62 <= getversion()) {
     byte ve = (*(byte (*)[64])0x7FFE0274)[21];
     printf("Virtualization is %sabled.\n", 1 == ve ? "en" : "dis");
     return 0;
